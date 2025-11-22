06bfe714fe443dd449d2ca87ddc6d7501824fc77
muslihan15 <ianmuslihan@gmail.com>
2025-11-22 20:38:17 +0800

    Refactor Navbar component and add e-Learning link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,142 +18,135 @@ import useScrollTrigger from "@mui/material/useScrollTrigger"
 import Slide from "@mui/material/Slide"
 
 function HideOnScroll(props) {
-	const { children, window } = props
-	// Note that you normally won't need to set the window ref as useScrollTrigger
-	// will default to window.
-	// This is only being set here because the demo is in an iframe.
-	const trigger = useScrollTrigger({
-		target: window ? window() : undefined,
-	})
+  const { children, window } = props
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  })
 
-	return (
-		<Slide appear={false} direction="down" in={!trigger}>
-			{children}
-		</Slide>
-	)
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  )
 }
 
 HideOnScroll.propTypes = {
-	children: PropTypes.element.isRequired,
-	/**
-	 * Injected by the documentation to work in an iframe.
-	 * You won't need it on your project.
-	 */
-	window: PropTypes.func,
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
 }
 
+// Menu List
 function Home() {
-	return <a href="#">HOME</a>
+  return <a href="#">HOME</a>
 }
-
 function About() {
-	return <a href="#About">About</a>
+  return <a href="#About">About</a>
 }
-
 function Contact() {
-	return <a href="#Contact">Contact</a>
+  return <a href="#Contact">Contact</a>
+}
+function ELearning() {
+  return (
+    <a href="https://fikomlearning.xyz" target="_blank" 
+       rel="noopener noreferrer">
+      e-Learning
+    </a>
+  )
 }
 
 const drawerWidth = 240
-const navItems = [<Home />, <About />, <Contact />]
+const navItems = [<Home />, <About />, <Contact />, <ELearning />]
 
 function DrawerAppBar(props) {
-	const { window } = props
-	const [mobileOpen, setMobileOpen] = React.useState(false)
+  const { window } = props
+  const [mobileOpen, setMobileOpen] = React.useState(false)
 
-	const handleDrawerToggle = () => {
-		setMobileOpen((prevState) => !prevState)
-	}
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState)
+  }
 
-	const drawer = (
-		<Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-			<Typography variant="h6" sx={{ my: 2 }} id="Navbar">
-				<b>IAN</b>
-			</Typography>
-			<Divider />
-			<List>
-				{navItems.map((item) => (
-					<ListItem key={item} disablePadding>
-						<ListItemButton sx={{ textAlign: "center" }}>
-							<ListItemText primary={item} />
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
-		</Box>
-	)
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }} id="Navbar">
+        <b>IAN</b>
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
 
-	const container = window !== undefined ? () => window().document.body : undefined
+  const container = window !== undefined ? () => window().document.body : undefined
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<CssBaseline />
-			<HideOnScroll {...props}>
-				<AppBar
-					component="nav"
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar
+          component="nav"
           sx={{
             backgroundColor: "rgba(0, 0, 0, 0)",
             backdropFilter: "blur(12px)",
-            px: ["2%", "8%"], // Responsive padding values
+            px: ["2%", "8%"],
             width: "100%",
             boxShadow: "none",
-          }}
-          >
-					<Toolbar sx={{ }}>
-						<IconButton
-							color="inherit"
-							aria-label="open drawer"
-							edge="start"
-							onClick={handleDrawerToggle}
-							sx={{ mr: 2, display: { sm: "none" } }}>
-							<MenuIcon />
-						</IconButton>
-						<Typography
-							variant="h6"
-							component="div"
-							sx={{ flexGrow: 1, display: { xs: "none", sm: "block", color: "#ced4d7" } }}
-							id="Navbar">
-							<b className="">ian</b>
-						</Typography>
-						<Box sx={{ display: { xs: "none", sm: "block" } }}>
-							{navItems.map((item) => (
-								<Button key={item} sx={{ color: "#ced4d7" }} id="Navbar">
-									{item}
-								</Button>
-							))}
-						</Box>
-					</Toolbar>
-				</AppBar>
-			</HideOnScroll>
-			<Box component="nav">
-				<Drawer
-					container={container}
-					variant="temporary"
-					open={mobileOpen}
-					onClose={handleDrawerToggle}
-					ModalProps={{
-						keepMounted: true, // Better open performance on mobile.
-					}}
-					sx={{
-						display: { xs: "block", sm: "none" },
-						"& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
-					}}>
-					{drawer}
-				</Drawer>
-			</Box>
-			<Box component="main" sx={{}}>
-				<Toolbar />
-			</Box>
-		</Box>
-	)
+          }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block", color: "#ced4d7" } }}
+              id="Navbar">
+              <b>IAN</b>
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item, index) => (
+                <Button key={index} sx={{ color: "#ced4d7" }} id="Navbar">
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          }}>
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main">
+        <Toolbar />
+      </Box>
+    </Box>
+  )
 }
 
 DrawerAppBar.propTypes = {
-	/**
-	 * Injected by the documentation to work in an iframe.
-	 * You won't need it on your project.
-	 */
-	window: PropTypes.func,
+  window: PropTypes.func,
 }
 
 export default DrawerAppBar
