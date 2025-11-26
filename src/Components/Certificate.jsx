95efeb6c17fa5611112d47b07738eebef5a8f094
muslihan15 <ianmuslihan@gmail.com>
2025-11-26 20:55:00 +0800

    Refactor Certificate component structure and styles
    Refactor Certificate component for improved readability and functionality.

--- src/Components/Certificate.jsx
+++ src/Components/Certificate.jsx
@@ -4,43 +4,43 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const Certificate = (props) => {
   const ImgSertif = props.ImgSertif;
+  const Title = props.Title;
   const [open, setOpen] = useState(false);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <div className="">
-        <img
-          src={ImgSertif}
-          alt="Img Sertif Eki"
-          id="ImgSertif"
-          onClick={handleOpen}
-          className="cursor-pointer "
-        />
-        
-      </div>
+      {/* Gambar Sertifikat */}
+      <img
+        src={ImgSertif}
+        alt={Title}
+        onClick={handleOpen}
+        className="cursor-pointer rounded-lg shadow-md hover:shadow-xl transition-all duration-200"
+      />
+
+      {/* Modal Pop Up */}
       <Modal open={open} onClose={handleClose}>
-        <div className="modal fixed inset-0 flex flex-col justify-center items-center bg-black bg-opacity-75">
+        <div className="modal fixed inset-0 flex flex-col justify-center items-center bg-black bg-opacity-80 p-5">
           <IconButton
             aria-label="close"
             onClick={handleClose}
             sx={{
-              position: 'absolute',
-              top: '0.5rem',
-              right: '0.5rem',
-              color: 'white'
+              position: "absolute",
+              top: "1rem",
+              right: "1rem",
+              color: "white"
             }}
           >
-            <CloseIcon sx={{ fontSize: 52 , position: "relative"}} />
+            <CloseIcon sx={{ fontSize: 50 }} />
           </IconButton>
-          <img src={ImgSertif} alt="Sertifikat Eki" className="max-w-full max-h-full " />
+
+          <img
+            src={ImgSertif}
+            alt={Title}
+            className="max-w-full max-h-full rounded-lg"
+          />
         </div>
       </Modal>
     </div>
